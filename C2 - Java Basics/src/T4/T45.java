307fd4182497a4c21276f6b287c4a628bc81cc38
CRivas <christianrivasp@gmail.com>
2021-02-02 20:18:46 +0100

    corrección t45

--- C2 - Java Basics/src/T4/T45.java
+++ C2 - Java Basics/src/T4/T45.java
@@ -14,12 +14,16 @@ public class T45 {
 	 */
 	public static void main(String[] args) {
 		int a=1, b=2, c=3, d=4;
-		System.out.println("Valores iniciales:\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d+"\n\n-------------------------------------------------\n");
+		System.out.println("Valores iniciales:\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d+"\n\n-------------------------------------------------");
 		b=c;
+		System.out.println("\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d);
 		c=a;
+		System.out.println("\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d);
 		a=d;
+		System.out.println("\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d);
 		d=b;
-		System.out.println("Valores finales:\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d);
+		System.out.println("\nA="+a+"\nB="+b+"\nC="+c+"\nD="+d);
+		
 	}
 
 }
