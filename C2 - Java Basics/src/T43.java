7b67d05f35283f2e2e9f8aedf84e7247bd3f8efa
CRivas <christianrivasp@gmail.com>
2021-02-02 18:15:14 +0100

    t43

---
+++ C2 - Java Basics/src/T43.java
@@ -0,0 +1,43 @@
+import java.util.Iterator;
+
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class T43 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=6;
+		int y=10;
+		double n=5.2;
+		double m=2.9;
+		System.out.println("Variables x="+x+", y="+y+", n="+n+", m="+m);
+		System.out.println("x+y="+(x+y));
+		System.out.println("x-y="+(x-y));
+		System.out.println("x*y="+(x*y));
+		System.out.println("x/y="+(x/y));
+		System.out.println("x%y="+(x%y));
+		System.out.println("n+m="+(n+m));
+		System.out.println("n-m="+(n-m));
+		System.out.println("n*m="+(n*m));
+		System.out.println("n/m="+(n/m));
+		System.out.println("n%m="+(n%m));
+		System.out.println("x+n="+(x+n));
+		System.out.println("y/m="+(y/m));
+		System.out.println("y%m="+(y%m));
+		System.out.println("x*2="+(x*2));
+		System.out.println("y*2="+(y*2));
+		System.out.println("n*2="+(n*2));
+		System.out.println("m*2="+(m*2));
+		System.out.println("x+y+n+m="+(x+y+n+m));
+		System.out.println("x*y*n*m="+(x*y*n*m));
+	}
+
+}
