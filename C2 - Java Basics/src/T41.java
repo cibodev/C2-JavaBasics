af3a37c10ca87b32be0e6a8af3f40b6ad5468f65
CRivas <christianrivasp@gmail.com>
2021-02-02 17:24:06 +0100

    T41

---
+++ C2 - Java Basics/src/T41.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class T41 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
