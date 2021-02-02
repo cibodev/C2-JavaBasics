8f92f67ef835f4a54ea703121aadda5e1cc2e86a
CRivas <christianrivasp@gmail.com>
2021-02-02 17:55:27 +0100

    t42

--- C2 - Java Basics/src/T41.java
+++ C2 - Java Basics/src/T41.java
@@ -12,7 +12,15 @@ public class T41 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int n1 = 41;
+		int n2 = 7;
+		
+		System.out.println("Estas son las operaciones que se pueden hacer con "+n1+" y "+n2+":");
+		System.out.println(n1+" + "+n2+" = "+(n1+n2));
+		System.out.println(n1+" - "+n2+" = "+(n1-n2));
+		System.out.println(n1+" x "+n2+" = "+(n1*n2));
+		System.out.println(n1+" / "+n2+" = "+(n1/n2));
+		System.out.println(n1+" % "+n2+" = "+(n1%n2));
 
 	}
 
---
+++ C2 - Java Basics/src/T42.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author User
+ *
+ */
+public class T42 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n=3;
+		double a=3.4;
+		char C='C';
+		int cascii= (int) C;
+		
+		System.out.println("Variables N="+n+", A="+a+" y C="+C);
+		System.out.println("N+A="+(n+a));
+		System.out.println("A-N="+(a-n));
+		System.out.println("Valor numerico de C ="+cascii);
+	}
+
+}
