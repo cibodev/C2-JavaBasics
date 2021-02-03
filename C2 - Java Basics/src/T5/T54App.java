44310a0fb3ac1efa87005f486416f04d74dfe4a6
CRivas <christianrivasp@gmail.com>
2021-02-03 16:11:21 +0100

    t54

--- C2 - Java Basics/src/T5/T54App.java
+++ C2 - Java Basics/src/T5/T54App.java
@@ -16,11 +16,12 @@ public class T54App {
 	/**
 	 * @param args
 	 */
-	static double PI =3.141592653589793238462;
+	static double PI =3.141592653589793238462;//Creo una constante global que sera pi ya que el numero pi no puede variar
 	public static void main(String[] args) {
+		//hacemos el parse double para que coja el string como double
 		double r = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el radio en mm"));
-		JOptionPane.showMessageDialog(null, "El area del circulo cuyo radio es "+r+" milimetros es: "+(PI*(r*r))+"milimetros cuadrados");
-
+		//multiplicamos PI por el radio al cuadrado y nos dara el area. Podria haber buscado como hacer elevaciones en java pero multiplicar r por si misma tambien funciona
+		JOptionPane.showMessageDialog(null, "El area del circulo cuyo radio es "+r+" milimetros es: "+(PI*(r*r))+" milimetros cuadrados");
 	}
 
 }
