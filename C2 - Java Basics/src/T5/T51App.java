b8373079fccd4b6dbaf41b8063eef78aa7568164
CRivas <christianrivasp@gmail.com>
2021-02-03 15:51:16 +0100

    t52

--- C2 - Java Basics/src/T5/T51App.java
+++ C2 - Java Basics/src/T5/T51App.java
@@ -15,10 +15,12 @@ public class T51App {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Introducimos los numeros por una ventana. Normalmente la ventana nos devolveria un string pero con un parseint podemos transformar ese string en un int
 		int n1= Integer.parseInt(JOptionPane.showInputDialog("Introduce el primer numero"));
 		int n2=Integer.parseInt(JOptionPane.showInputDialog("Introduce el segundo numero"));;
+		//primer if para ver si los numeros son distintos, si no lo son sabremos que son iguales
 		if (n1!=n2) {
-			if (n1>n2) {
+			if (n1>n2) {//si son diferentes pasaran a este ifelse que diferenciara cual de los dos es mas grande
 				JOptionPane.showMessageDialog(null, n1+" es mas grande que "+n2);
 			} else {
 				JOptionPane.showMessageDialog(null, n1+" es mas pequeño que "+n2);
---
+++ C2 - Java Basics/src/T5/T52App.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package T5;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author User
+ *
+ */
+public class T52App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String name = "Christian";
+		JOptionPane.showMessageDialog(null, "Bienvenido "+name);
+	}
+
+}
