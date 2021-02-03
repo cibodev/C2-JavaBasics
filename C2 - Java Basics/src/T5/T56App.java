f14cd3534810b6f71d568b9d85ecab6dd5aa13ee
CRivas <christianrivasp@gmail.com>
2021-02-03 16:31:15 +0100

    t57

--- C2 - Java Basics/src/T5/T56App.java
+++ C2 - Java Basics/src/T5/T56App.java
@@ -18,7 +18,7 @@ public class T56App {
 	 */
 	static double IVA=1.21;//creo la variable global IVA que sera 1.21 ya que al multiplicarla por el precio del producto me dara el precio del producto mas iva
 	public static void main(String[] args) {
-		//Hacemos el parse double para tener el tipo de datoq ue queremos
+		//Hacemos el parse double para tener el tipo de dato que queremos
 		double n= Double.parseDouble(JOptionPane.showInputDialog("Introduce el precio de el producto"));
 		JOptionPane.showMessageDialog(null, "El precio del producto mas IVA es "+(IVA*n));
 	}
---
+++ C2 - Java Basics/src/T5/T57App.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package T5;
+
+/**
+ * @author Christian Rivas Pottier 
+ * 
+ * 3 feb. 2021
+ *
+ */
+public class T57App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n=0;
+		while (n<100) {//creo un bucle con while que parara cuando n llegue a 100
+			n++;//Al poner el n++ antes del print hago que su valor sea mayor en uno al que se valida en el while
+			System.out.println(n);//en este ejercicio usare terminal porque aun no hemos hacho cadenas y no quiero que salgan 100 ventanitas diferentes
+		}
+	}
+}
