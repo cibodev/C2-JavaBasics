12afcbf5a5342841c1d72913f560211725618224
CRivas <christianrivasp@gmail.com>
2021-02-05 17:35:03 +0100

    t604

---
+++ C2 - Java Basics/src/T6/T604App.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package T6;
+
+import java.lang.reflect.Array;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Christian Rivas Pottier 
+ * 
+ * 5 feb. 2021
+ *
+ */
+public class T604App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n= Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero"));
+		factorial(n);
+	}
+	public static void factorial(int n) {
+		long fn=1;
+		for (int i = 1; i < n; i++) {
+			fn=fn+(fn*i);
+		}
+		JOptionPane.showMessageDialog(null, "El factorial de "+n+" es "+fn);
+	}
+}
